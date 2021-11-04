cbc631ad58981b5b957a0b7f662798e7b8e13c33
kuy yngd <yanacod@gmail.com>
2021-11-04 15:12:02 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import serial
+import time
+import requests
+import json
+
+def main():
+  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+  ser.flush()
+  url = 'http://192.168.32.15:8000/api/insert_data'
+
+  while True:
+    time.sleep(1)
+    line = ser.readline().decode('utf-8')
+    line = line.rstrip()
+    print(line)
+    send_data = { "send_data": line }
+    headers = {"content-type": "application/json"}
+    res = requests.post(url, data = json.dumps(send_data), headers = headers, verify = False)
+    print(res.text)
+    print(res)
+
+main()
